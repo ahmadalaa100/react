6caef93273007e6f72900f22f6152051382d27bd
Ahmed Alaa <ahmedalaamoussa@outlook.com>
2021-03-17 13:50:43 +0200

    lab1

---
+++ src/increment.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Increment extends Component {
+    state = { 
+        count:0
+     }
+     incrementhandeler = () => {
+        this.setState({count : this.state.count+1});
+    }
+    decrementhandeler = () =>{
+        this.setState({count : this.state.count-1});
+      }
+    render() { 
+        return (  
+            <div>
+              <h2>  <span >{this.state.count}</span> </h2>  
+                <br/>
+                <button onClick={() => {this.incrementhandeler()}} >add</button>
+                <button onClick={() => {this.decrementhandeler()}} >remove</button>
+            </div>
+
+        );
+    }
+}
+ 
+export default Increment;
